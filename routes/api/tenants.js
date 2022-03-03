6b34f7904c45001dd7ce6de02fe830a2a442f781
dev123-dev <dev@pinnaclemedia.in>
2022-03-03 11:29:20 +0530

    prathiksha

--- routes/api/tenants.js
+++ routes/api/tenants.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 const mongoose = require("mongoose");
-const StaffPerf = require("../../models/staffPerformence");
+//const StaffPerf = require("../../models/staffPerformence");
 const TenantDetails = require("../../models/TenantDetails");
 const TenantSettings = require("../../models/TenantSettings");
 const ShopDetails = require("../../models/ShopDetails");
+const TenantAgreementDetails = require("../../models/TenantAgreementDetails");
 
 router.post("/add-tenant-details", async (req, res) => {
   let data = req.body;
-  console.log(data);
+
   try {
     let tenantDetails = new TenantDetails(data);
     output = await tenantDetails.save();
@@ -72,4 +73,97 @@ router.get("/get-tenant-report", async (req, res) => {
   }
 });
 
+router.post(
+  "/deactive-tenant",
+  // [check("tdId", "Invalid Request").not().isEmpty()],
+
+  async (req, res) => {
+    console.log("api");
+    try {
+      let data = req.body;
+
+      const updatedetails = await TenantDetails.updateOne(
+        { _id: data.recordId },
+        {
+          $set: {
+            tenantstatus: data.tenantstatus,
+            tenantdeactivereason: data.tenantdeactivereason,
+          },
+        }
+      );
+
+      res.json(updatedetails);
+    } catch (error) {
+      res.status(500).json({ errors: [{ msg: "Server Error" }] });
+    }
+  }
+);
+
+// router.post(
+//   "/deactive-tenant",
+//   // [check("tdId", "Invalid Request").not().isEmpty()],
+//   async (req, res) => {
+//     console.log("hi api");
+//     try {
+//       let data = req.body;
+//       console.log(data);
+//       const updatedetails = await Tenantdetails.updateOne(
+//         { _id: data.tdId },
+//         {
+//           $set: {
+//             tenantstatus: data.tenantstatus,
+//           },
+//         }
+//       );
+//       //console.log(data);
+//       console.log(updatedetails);
+//       res.json(updatedetails);
+//     } catch (error) {
+//       res.status(500).json({ errors: [{ msg: "Server Error" }] });
+//     }
+//   }
+// );
+
+router.post("/add-agreement-details", async (req, res) => {
+  let data = req.body;
+  console.log(data);
+  try {
+    let tenantAgreementDetails = new TenantAgreementDetails(data);
+    output = await tenantAgreementDetails.save();
+    res.send(output);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal Server Error.");
+  }
+});
+
+router.get("/get-all-levels", async (req, res) => {
+  try {
+    const staffLevelData = await ShopDetails.find({});
+    res.json(staffLevelData);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal Server Error.");
+  }
+});
+
+router.get("/get-all-tenants", async (req, res) => {
+  try {
+    const tenanatData = await TenantDetails.find({});
+    res.json(tenanatData);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal Server Error.");
+  }
+});
+
+router.get("/get-all-settings", async (req, res) => {
+  try {
+    const tenanatSettingData = await TenantSettings.find({});
+    res.json(tenanatSettingData);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal Server Error.");
+  }
+});
 module.exports = router;
